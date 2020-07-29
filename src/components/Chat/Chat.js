d4169648c5b9315d078fd51672795d90401c4a6b
Madrigalalex <AZM0208@tigermail.auburn.edu>
2020-07-29 16:10:03 -0700

    added a info bar componenet and CSS to it.

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
 
 let socket;
 
@@ -8,7 +9,7 @@ const Chat = ({location}) => {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const [message, setMessages] = useState('');
+    const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
@@ -33,12 +34,28 @@ const Chat = ({location}) => {
     }, [ENDPOINT, location.seacrh]);
 
     useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message])
+        })        
+    }, [messages]);
 
-        
-    })
+    //function to send messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    } 
+
+    console.log(message, messages);
 
     return(
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input value={message} onChange={(event) => setMessage(event.target.value)} 
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) :  null} />
+            </div>
+        </div>
     )
 
 }
---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './InfoBar.css';
+
+
+const InfoBar = () => {
+    <div className="infoBar">
+        <div className="leftInnerContainer"> 
+        
+        </div>
+        <div className="rightInnerContainer"> 
+        
+        </div>
+    </div>
+
+
+}
+
+export default InfoBar;
