89274dada7bd1df2400220903d15c1b687473d00
Madrigalalex <AZM0208@tigermail.auburn.edu>
2020-07-31 10:33:46 -0700

    fix build issues

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -24,8 +24,10 @@ const Chat = ({location}) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, () => {
-            
+        socket.emit('join', {name, room}, (error) => {
+            if(error){
+                alert(error);
+            }
         });
 
         return () => {
